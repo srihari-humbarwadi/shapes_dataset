ad5a0e4a7265de020cd0889f4daa10c85d4868ed
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-06-02 04:36:17 +0530

    fixed bugs

--- generate_shapes_dataset.py
+++ generate_shapes_dataset.py
@@ -1,11 +1,44 @@
-import json
-import os
-
-import cv2
 import numpy as np
-from tqdm import tqdm
+import os
+import cv2
+import json
+import matplotlib.pyplot as plt
+from skimage.io import imsave, imread
+from tqdm.notebook import tqdm
 
-from skimage.io import imsave
+
+def imshow(image):
+    plt.figure(figsize=(8, 6))
+    plt.imshow(image)
+
+
+def draw_boxes_cv2(image, boxes, categories):
+    boxes = np.array(boxes, dtype=np.int32)
+    for _box, _cls in zip(boxes, categories):
+        text = _cls
+        char_len = len(text) * 9
+        text_orig = (_box[0] + 5, _box[1] - 6)
+        text_bg_xy1 = (_box[0], _box[1] - 20)
+        text_bg_xy2 = (_box[0] + char_len, _box[1])
+        image = cv2.rectangle(image, text_bg_xy1, text_bg_xy2, [255, 252, 150],
+                              -1)
+        image = cv2.putText(image,
+                            text,
+                            text_orig,
+                            cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                            .6, [0, 0, 0],
+                            5,
+                            lineType=cv2.LINE_AA)
+        img = cv2.putText(image,
+                          text,
+                          text_orig,
+                          cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                          .6, [255, 255, 255],
+                          1,
+                          lineType=cv2.LINE_AA)
+        img = cv2.rectangle(image, (_box[0], _box[1]), (_box[2], _box[3]),
+                            [30, 15, 30], 1)
+    return img
 
 
 def convert_box(box, out_format):
@@ -29,7 +62,7 @@ def convert_box(box, out_format):
 
 def draw_circle(rgb_canvas, trials=1):
     if trials > 100:
-        return rgb_canvas
+        return rgb_canvas, {}
     H, W = rgb_canvas.shape[:2]
 
     canvas = np.uint8(~np.all(rgb_canvas == [255, 255, 255], axis=-1))
@@ -53,7 +86,7 @@ def draw_circle(rgb_canvas, trials=1):
         return draw_circle(rgb_canvas, trials=trials + 1)
 
     colors = [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
-    color = colors[np.random.randint(0, 3)]
+    color = list(colors[np.random.randint(0, 3)])
 
     x1, y1, x2, y2 = map(
         int, convert_box([x, y, 2 * radius, 2 * radius], out_format='x1y1x2y2'))
@@ -70,7 +103,7 @@ def draw_circle(rgb_canvas, trials=1):
 
 def draw_rectangle(rgb_canvas, trials=0):
     if trials > 100:
-        return canvas
+        return rgb_canvas, {}
 
     canvas = np.uint8(~np.all(rgb_canvas == [255, 255, 255], axis=-1))
 
@@ -98,7 +131,7 @@ def draw_rectangle(rgb_canvas, trials=0):
         return draw_rectangle(rgb_canvas, trials=trials + 1)
 
     colors = [[255, 0, 0], [0, 255, 0], [0, 0, 255]]
-    color = colors[np.random.randint(0, 3)]
+    color = list(colors[np.random.randint(0, 3)])
 
     return cv2.rectangle(rgb_canvas,
                          pt1=(x1, y1),
@@ -112,7 +145,7 @@ def draw_rectangle(rgb_canvas, trials=0):
 
 def create_dataset(num_samples,
                    image_shape,
-                   max_objects=6,
+                   max_objects=10,
                    draw_fns=[draw_circle, draw_rectangle],
                    save_dir=None):
     if not os.path.exists(save_dir):
@@ -128,11 +161,13 @@ def create_dataset(num_samples,
         objects = []
         image_path = os.path.join(save_dir, 'images', image_name)
         rgb_canvas = np.ones(shape=[h, w, 3], dtype=np.uint8) * 255
-        num_objects = np.random.randint(low=1, high=max_objects + 1)
+        num_objects = np.random.randint(low=2, high=max_objects + 1)
 
         for i in range(num_objects):
             fn = np.random.choice(draw_fns)
             canvas, annotation = fn(rgb_canvas)
+            if annotation == {}:
+                continue
             objects.append(annotation)
 
         imsave(image_path, rgb_canvas, check_contrast=False)
@@ -144,6 +179,20 @@ def create_dataset(num_samples,
 
 
 if __name__ == '__main__':
-    h, w = 448, 448
-    num_samples = 3000
-    dataset = create_dataset(num_samples, [h, w], max_objects=5, save_dir='../tutorials/data/shapes_dataset')
+    h = w = sys.argv[1]
+    max_objects = sys.argv[2]
+    num_samples = 12500
+    dataset = create_dataset(num_samples, [h, w], max_objects=max_objects), save_dir = 'data')
+
+    # for i, (image_path, annotation) in enumerate(dataset.items()):
+    #     boxes=[]
+    #     categories=[]
+    #     for obj in annotation:
+    #         boxes.append(obj['box'])
+    #         categories.append(obj['category'])
+
+    #     image=imread('data/images/' + image_path)
+    #     image=draw_boxes_cv2(image, boxes, categories)
+    #     imshow(image)
+    #     if i == 15:
+    #         break
